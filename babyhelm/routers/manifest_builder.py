f2f0e2535409008b9f466645696883884399d05d
Iskander Shamsutdinov <wensietyt@gmail.com>
2024-06-19 14:48:01 +0300

    refactor(manifest-builder): generalized manifest builder response

--- babyhelm/routers/manifest_builder.py
+++ babyhelm/routers/manifest_builder.py
@@ -5,7 +5,7 @@ from starlette import status
 from starlette.responses import JSONResponse
 
 from babyhelm.containers.application import ApplicationContainer
-from babyhelm.schemas.manifest_builder import Values
+from babyhelm.schemas.manifest_builder import Application, Values
 from babyhelm.services.manifest_builder import ManifestBuilderService
 
 router = APIRouter(prefix="/manifests", tags=["Manifests"])
@@ -14,18 +14,9 @@ router = APIRouter(prefix="/manifests", tags=["Manifests"])
 @router.post("/render")
 @inject
 async def render_manifests(
-        values: Values,
+        application: Application,
         manifest_builder_service: ManifestBuilderService = Depends(
                 Provide[ApplicationContainer.services.manifest_builder]
         )
 ) -> JSONResponse:
-    deployment_manifest = manifest_builder_service.render_deployment_manifest(values)
-    service_manifest = manifest_builder_service.render_service_manifest(values)
-    return JSONResponse(
-            content={
-                "message": "Manifests rendered",
-                "deployment": yaml.safe_load(deployment_manifest),
-                "service": yaml.safe_load(service_manifest)
-            },
-            status_code=status.HTTP_200_OK
-    )
+    return manifest_builder_service.render_application(application)
