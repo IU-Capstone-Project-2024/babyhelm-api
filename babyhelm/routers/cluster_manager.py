4dd78ad5d4805fc1425a50aee9ef92f4ab17074f
Egor Salnikov <4182808@gmail.com>
2024-06-28 12:54:34 +0300

    fix(routers): add status code 201

--- babyhelm/routers/cluster_manager.py
+++ babyhelm/routers/cluster_manager.py
@@ -1,5 +1,5 @@
 from dependency_injector.wiring import Provide, inject
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status
 
 from babyhelm.containers.application import ApplicationContainer
 from babyhelm.schemas.cluster_manager import (
@@ -13,7 +13,7 @@ from babyhelm.services.cluster_manager import ClusterManagerService
 router = APIRouter(prefix="/clusters", tags=["Clusters"])
 
 
-@router.post("/create-project")
+@router.post("/create-project", status_code=status.HTTP_201_CREATED)
 @inject
 async def create_project(
     project: Project,
@@ -24,7 +24,7 @@ async def create_project(
     return await cluster_manager_service.create_project(project)
 
 
-@router.post("/create-application")
+@router.post("/create-application", status_code=status.HTTP_201_CREATED)
 @inject
 async def create_application(
     app: CreateApplicationRequest,
